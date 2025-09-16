d1312905809cc2bff90610a754d744810af3d236
ivanorellana90 <ivan990@gmail.com>
2025-09-16 17:19:54 -0300

    feat(Dashboard): add DevicesByVersionChart component and update OverviewTab layout fix(DevicesByModeChart, ClientDevicesTable): update header styles for consistency

--- src/jsx/components/Dashboard/Data/DevicesByModeChart.jsx
+++ src/jsx/components/Dashboard/Data/DevicesByModeChart.jsx
@@ -80,7 +80,7 @@ export default function DevicesByModeChart({ devices }) {
   return (
     <div className="card">
       <div className="card-header">
-        <h4 className="card-title">Devices by Mode</h4>
+        <h4 className="text-black mb-0">Devices by Mode</h4>
       </div>
       <div className="card-body">
         <div className="row mx-0 align-items-center">
